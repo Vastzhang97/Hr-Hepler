c5d5ae078aee40d0a8b38f8d208f47a15caefba9
WaterZhang97 <31640986+WaterZhang97@users.noreply.github.com>
2019-10-07 22:38:34 +0800

    增加复制带不同关键字文件到对应文件夹

--- moveFile.py
+++ moveFile.py
@@ -4,24 +4,31 @@ import os, shutil
 if __name__ == '__main__':
 
     # 源文件夹路径
-    source_file_path = 'C:/Users/62526/Desktop/Test'
+    source_file_path = '/Users/chenjiaqian/Desktop/未命名文件夹/未命名文件夹'
     # 输出文件夹路径
-    target_file_path = 'C:/Users/62526/Desktop/Test1'
+    target_file_path = '/Users/chenjiaqian/Desktop/未命名文件夹'
+    target_dir = ['1', '2', '3', '4', '5']
+
     # 关键字
-    key_word = 'test'
+    key_words = ['1', '2', '3', '4', '5']
 
-    # 如果输出文件夹路径不存在(百度 Python3 判断文件和文件夹是否存在、创建文件夹)
-    if not os.path.exists(target_file_path):
-        # 创建文件夹(百度 Python3 创建文件夹)
-        os.mkdir(target_file_path)
+    for index, key in enumerate(key_words):
 
-    # 遍历输出文件夹路径下的所有文件(百度 Python3:遍历某个目录下的所有文件)
-    for parent, dir_names, file_names in os.walk(source_file_path, followlinks=True):
-        # 遍历输出文件夹路径下的所有文件名字
-        for filename in file_names:
-            # 每一个文件的名字
-            file_path = os.path.join(parent, filename)
-            # 如果文件名里有关键字
-            if key_word in filename:
-                # 复制该文件到输出文件夹
-                path = shutil.copy(file_path, target_file_path + '/' + filename)
+        target_dir_path = target_file_path + '/' + target_dir[index]
+        # 如果输出文件夹路径不存在(百度 Python3 判断文件和文件夹是否存在、创建文件夹)
+        if not os.path.exists(target_dir_path):
+            # 创建文件夹(百度 Python3 创建文件夹)
+            os.mkdir(target_dir_path)
+
+        # 遍历输出文件夹路径下的所有文件(百度 Python3:遍历某个目录下的所有文件)
+        for parent, dir_names, file_names in os.walk(source_file_path, followlinks=True):
+            # 遍历输出文件夹路径下的所有文件名字
+            for filename in file_names:
+                # 每一个文件的名字
+                file_path = os.path.join(parent, filename)
+                # 如果文件名里有关键字
+                if key_words[index] in filename:
+                # if filename.startswith(key_words[index]):
+                    # 复制该文件到输出文件夹
+                    path = target_file_path + '/' + target_dir[index] + '/' + filename
+                    shutil.copy(file_path, path)
